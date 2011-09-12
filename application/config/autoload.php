c7f132d5322ef550873e20498aba9132d1423555
Hilla <hillamed3@gmail.com>
2011-09-12 16:22:51 +0200

    changing helpers acording to new spec

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,8 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('SimpleLoginSecure', 'sessions', 'form_validation'
-);
+$autoload['libraries'] = array('SimpleLoginSecure', 'session');
 
 
 /*
@@ -65,7 +64,7 @@ $autoload['libraries'] = array('SimpleLoginSecure', 'sessions', 'form_validation
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('file_helper');
+$autoload['helper'] = array();
 
 
 /*
